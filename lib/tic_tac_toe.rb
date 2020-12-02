ae1224217f523b552469fb32f138d07958049b0e
Alison Williams <apw5011@gmail.com>
2020-12-02 00:10:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,14 +5,14 @@ class TicTacToe
   end 
   
   WIN_COMBINATIONS = [
-    [1,2,3], # top row 
+    [0,1,2], # top row 
     [3,4,5], # middle row 
     [6,7,8], # bottom row
-    [1,3,6], # first column
-    [2,4,7], # second column 
-    [3,5,8], # third column 
-    [1,4,8], # diagonal 1 
-    [3,4,6] # diagonal 2 
+    [0,3,6], # first column
+    [1,4,7], # second column 
+    [2,5,8], # third column 
+    [0,4,8], # diagonal 1 
+    [2,4,6] # diagonal 2 
     ]
   
 end 
