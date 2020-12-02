d0d9e5eed221492dc0974db846fdb4f86e0f453d
Alison Williams <apw5011@gmail.com>
2020-12-02 00:53:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,6 +66,7 @@ class TicTacToe
     input = gets.strip 
     index = input_to_index(input)
     if valid_move?(index)
+      token = current_player
       move(index, token)
       display_board
     else 
