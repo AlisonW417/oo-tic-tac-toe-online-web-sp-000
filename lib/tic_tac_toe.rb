d765c6ba4397695efd97cf5b53c7f0480ca6eca3
Alison Williams <apw5011@gmail.com>
2020-12-02 00:15:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,10 @@ class TicTacToe
     ]
   
   def display_board
-    puts 
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "-----------"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]"
+    puts "-----------
   end 
   
 end 
